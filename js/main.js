354bf327675e32cb5b22c10cfd687bea679b08f4
Priyanta Islam <priyantaislam@gmail.com>
2022-07-14 09:06:00 +1000

    added input system

--- js/main.js
+++ js/main.js
@@ -5,11 +5,34 @@ document.addEventListener("DOMContentLoaded", () =>{
         const guessedWords = [[]];
         let availableSpace = 1;
 
+        let word = "swear";
+
         const keys = document.querySelectorAll(".keyboard-row button")
 
         
         //******FUNCTIONS******//
-        
+        function handleSubmitWord() {
+                const currentWordArr = getCurrentWordArr();
+                if(currentWordArr.length != 5) {
+                        window.alert("Word must be 5 letters!");
+
+                }
+
+                const currentWord = currentWordArr.join('');
+
+                if(currentWord === word) {
+                        window.alert("Congratulations!");
+                }
+
+                if(guessedWords.length === 6 && currentWordArr.length == 5) {
+                        window.alert(`Sorry, you have no more guesses! The word is ${word}.`);
+                }
+
+                
+                guessedWords.push([]);
+        }
+
+
         function getCurrentWordArr () {
                 const numOfGuess = guessedWords.length;
                 return guessedWords[numOfGuess - 1];
@@ -20,12 +43,12 @@ document.addEventListener("DOMContentLoaded", () =>{
 
                 if(currentWordArr && currentWordArr.length < 5) {
                         currentWordArr.push(letter);
-                }
 
-                const availableSpaceEl = document.getElementById(String(availableSpace));
-                availableSpace = availableSpace + 1;
+                        const availableSpaceEl = document.getElementById(String(availableSpace));
+                        availableSpace = availableSpace + 1;
                 
-                availableSpaceEl.textContent = letter;
+                        availableSpaceEl.textContent = letter;
+                }      
 
         }
 
@@ -48,6 +71,12 @@ document.addEventListener("DOMContentLoaded", () =>{
                 keys[i].onclick = ({target}) => {
                         const key = target.getAttribute("data-key");
                         //console.log(key);
+                        
+                        if(key === 'enter') {
+                                handleSubmitWord();
+                                return;
+                        }
+
                         updateGuessedWord(key);
                 }
                 
